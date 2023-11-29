bdcb42b2df3b0fbf29489e4a48e48468acf98825
Mudassir5367 <mudassirhussain32303@gmail.com>
2023-11-29 14:28:02 +0500

    connection,frintend,backend,mongoos

--- backend/backed.js
+++ backend/backed.js
@@ -1,21 +1,52 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const port = 5000
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const port = 5000;
 
-const arr = []; 
-app.use(express.json())
-app.use(cors())
- app.get('/',(req,res)=>{
-    res.send(arr)
- })
+mongoose.connect('mongodb://localhost:27017/assignmet-connection', {});
 
- app.post('/form',(req,res)=>{
-    res.send('form')
-    console.log(req.body)
-    arr.push(req.body)
- })
+const db = mongoose.connection;
+db.on('error', console.error, 'MongoDB connection error:');
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
- app.listen(port,()=>{
-    console.log(`App is running on port ${port}`);
- })
+const formDataSchema = new mongoose.Schema({
+  name: String,
+  age: String,
+  email: String,
+});
+
+const FormData = mongoose.model('FormData', formDataSchema);
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', async (req, res) => {
+  try {
+    const data = await FormData.find();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.send(' Error');
+  }
+});
+
+app.post('/form', async (req, res) => {
+  try {
+    const { name, age, email } = req.body;
+
+    const formData = new FormData({ name, age, email });
+    await formData.save();
+
+    res.send('Form data saved to MongoDB');
+  } catch (error) {
+    console.error(error);
+    res.send(' Error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App is running on port ${port}`);
+});
